16871627006a5211ac80ba0ee1cf3c6d0d2be0c1
Cody Hopper <hello@codyhopper.com>
2025-02-18 12:57:34 -0500

    fix some metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,45 @@
 import type { Metadata } from "next";
+import { openGraphImage } from '../lib/shared-metadata';
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "../providers/ThemeProvider";
+import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Made—Type Foundry",
-  description: "Bespoke typefaces drawn from everyday phenomena.",
+    title: "made–type Foundry",
+    description: "Bespoke typefaces drawn from everyday phenomena.",
+    openGraph: {
+        ...openGraphImage,
+        title: "made–type Foundry",
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        id="main-content"
-        className={`
-            text-zinc-800 bg-zinc-100
-            dark:text-zinc-200 dark:bg-zinc-950
-            ${geistSans.variable} ${geistMono.variable} antialiased`
-        }
-      >
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                id="main-content"
+                className={`
+                    text-zinc-800 bg-zinc-100
+                    dark:text-zinc-200 dark:bg-zinc-950
+                    ${geistSans.variable} ${geistMono.variable} antialiased`
+                }
+            >
+                <ThemeProvider>{children}</ThemeProvider>
+            </body>
+        </html>
+    );
 }
---
+++ src/lib/shared-metadata.tsx
@@ -0,0 +1,10 @@
+export const openGraphImage = {
+    images: [
+        {
+            url: "/og-image.png",
+            width: 800,
+            height: 600,
+            alt: "An image description",
+        },
+    ],
+}
